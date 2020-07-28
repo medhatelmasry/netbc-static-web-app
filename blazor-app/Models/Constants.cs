d8ad4326fb42c97b6c5e07cc43efc633b5871949
Medhat Elmasry <medhat@elmasry.ca>
2020-07-28 12:38:26 -0700

    IntToStringConverter

--- blazor-app/Models/Constants.cs
+++ blazor-app/Models/Constants.cs
@@ -2,8 +2,8 @@ namespace blazor_app.Models {
     public class Constants {
         public static string BaseURL {
             get {
-                //return "http://localhost:7071/";
-                return "/";
+                return "http://localhost:7071/";
+                //return "/";
             }
         }
     }
--- blazor-app/Models/Product.cs
+++ blazor-app/Models/Product.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace blazor_app.Models {
     public class Product {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        //public int Quantity { get; set; }
+        [JsonConverter(typeof(IntToStringConverter))]
+        public int Quantity { get; set; }
     }
 }
